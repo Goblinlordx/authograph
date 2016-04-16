21d32dd3a2d0c483a38d4ac77b44576c8fcebeb4
Ben Baldivia <goblinlordx@gmail.com>
2016-04-16 14:53:42 +0900

    Add test set for bounds checking functions

--- src/core/__tests__/bounds-test.js
+++ src/core/__tests__/bounds-test.js
@@ -19,8 +19,48 @@ import {expect} from 'chai';
 
 describe('Bound function tests:', () => {
   describe('min',() => {
-    it('passes when recieved argument is not received', () => {
+    it('passes when argument is not received', () => {
       expect(min('testArg', 2, null, {})).to.equal(true);
+    });
+    it('passes when argument equal to min', () => {
+      expect(min('testArg', 2, null, {testArg: 2})).to.equal(true);
+    });
+    it('passes when argument greater than min', () => {
+      expect(min('testArg', 2, null, {testArg: 3})).to.equal(true);
+    });
+    it('fails when argument less than min', () => {
+      expect(min('testArg', 2, null, {testArg: 1})).to.equal(false);
+    });
+  });
+  describe('max', () => {
+    it('passes when argument is not received', () => {
+      expect(max('testArg', 2, null, {})).to.equal(true);
+    });
+    it('passes when argument equal to max', () => {
+      expect(max('testArg', 2, null, {testArg: 2})).to.equal(true);
+    });
+    it('fails when argument greater than max', () => {
+      expect(max('testArg', 2, null, {testArg: 3})).to.equal(false);
+    });
+    it('passes when argument less than max', () => {
+      expect(max('testArg', 2, null, {testArg: 1})).to.equal(true);
+    });
+  });
+  describe('oneOf', () => {
+    it('passes when argument is not received', () => {
+      expect(oneOf('testArg', 2, null, {})).to.equal(true);
+    });
+    it('passes when argument value is in set', () => {
+      expect(oneOf('testArg', [ 'test', 'asdf' ],
+                   null, {testArg: 'asdf'})).to.equal(true);
+    });
+    it('passes when argument value is single value and not array', () => {
+      expect(oneOf('testArg', 'asdf',
+                   null, {testArg: 'asdf'})).to.equal(true);
+    });
+    it('fails when argument is not in set', () => {
+      expect(oneOf('testArg', [ 'test', 'asdf' ],
+                   null, {testArg: 'fail'})).to.equal(false);
     });
   });
 });
